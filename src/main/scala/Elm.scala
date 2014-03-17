40df8cd35a069e23e2efa5061eee2b128ea7693d
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-03-17 19:12:18 +0100

    add Elm.Scala, which contains code for generating code. Main.scala, simple main application

---
+++ src/main/scala/Elm.scala
@@ -0,0 +1,57 @@
+import scala.collection.mutable.MutableList
+import scala.collection.mutable.StringBuilder
+
+class Program {
+  private var mapping : Map[Int, Expression] = Map[Int, Expression]()
+  private var id : Int = -1
+
+  def addExpr(expr : Expression) : Int = {
+    id += 1
+    mapping += id -> expr
+    return id
+  }
+
+  def emit : String = {
+    val str = new StringBuilder
+    str ++= Constants.ElmHeader
+    str ++= indent(3, "var _op = {};\n")
+    
+    val prog = genProgram
+    
+    prog.foreach { str ++= indent(3, _) }
+
+    str ++= indent(3, "_elm.Main.values = {_op: _op")
+
+    prog.foreach { 
+      case InitS(n, _) =>
+        str ++= "\n" + indent(22, "," + n + ": " + n)
+      case ImportS(_, _) => ()
+    }
+    str ++= "};\n" + Constants.ElmFooter
+    return str.toString
+  }
+
+  private def indent(depth : Int, s : String) : String = 
+    " " * depth + s
+
+  private def genProgram : List[Statement] = {
+    val stmts = MutableList[Statement]()
+    val program = mapping.init
+
+    program.foreach {
+      case (n, expr) =>
+        val name = "x" + n
+        stmts += InitS(name, expr)
+    }
+
+    stmts += InitS("main", mapping.last._2)
+
+    return stmts.toList
+  }
+}
+
+class Elm {
+  val prog : Program = new Program()
+  def __newVar[T](expr : Expression) : Int = prog.addExpr(expr)
+  implicit def intToExp(i : Int) : Expression = NumE(i)
+}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,12 @@
+object Main {
+  def main(args : Array[String]) {
+    println("Initial")
+
+    object Example extends Elm {
+      var a = 1
+      var b = 2
+    }
+
+    println(Example.prog.emit)
+  }
+}

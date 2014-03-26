26b0a5b7f3dbf487351be187a17e1e42754c4266
David Spångberg <david@tunna.org>
2014-03-26 16:54:02 +0100

    initial experiments with elm module parser

---
+++ src/main/scala/elm/ElmParser.scala
@@ -0,0 +1,40 @@
+package elm
+
+import scala.util.parsing.combinator._
+
+object ElmParser extends RegexParsers {
+
+  override val skipWhitespace = false
+
+  val ident: Parser[String] = "[a-z][A-Za-z0-9_]*".r
+
+  val comments: Parser[Unit] = {
+    lazy val f: Parser[Unit] = { ("-}" | "(?s).".r ~> f) ~> success(Unit) }
+
+    val mc: Parser[Unit] = "{-" ~> f ~> success(Unit)
+    val sc: Parser[Unit] = "--[^\n]*".r ~> success(Unit)
+
+    rep (sc | mc) >> {
+      case Nil    => success(Unit)
+      case _ :: _ => "\n" ~> success(Unit)
+    }
+  }
+
+  val moduleName: Parser[Name] = "module " ~> ident <~ " where"
+
+  val elmModule: Parser[ElmModule] = {
+    val name = comments ~> moduleName
+
+    comments ~> (name ^^ (n => (n, List(), List())))
+  }
+
+  def parseElm(s: String) = parse(elmModule, s)
+
+  type Name = String
+
+  sealed class Import(s: Name, open: Boolean)
+  sealed class FunDef(s: Name, ty: Type)
+
+  type ElmModule = (Name, List[Import], List[FunDef])
+
+}

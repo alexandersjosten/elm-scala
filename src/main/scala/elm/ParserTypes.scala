bd04e58aca84c616002d82ec41421840a3604740
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-04-16 14:22:42 +0200

    types for the parser

---
+++ src/main/scala/elm/ParserTypes.scala
@@ -0,0 +1,8 @@
+package elm
+
+sealed abstract class ParserType {
+  // Possibly add toString..
+}
+case class TupleType(ts: List[ParserType]) extends ParserType
+case class Var(s: String) extends ParserType
+case class Fun(ts: List[ParserType]) extends ParserType

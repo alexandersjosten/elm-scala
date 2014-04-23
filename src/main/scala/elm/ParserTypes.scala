dfac6d526f874acd1e9ab35251851cf64f23448b
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-04-23 15:58:41 +0200

    added implicit string represenation for parsertypes

--- src/main/scala/elm/ParserTypes.scala
+++ src/main/scala/elm/ParserTypes.scala
@@ -1,10 +1,25 @@
 package elm
 
 sealed abstract class ParserType {
-  // Possibly add toString..
+  implicit def stringVal(pt: ParserType): String = {
+    pt match {
+      case VarP(s)       => s
+      case TupleType(ts) => "Expr[(" + ts.mkString(",") + ")]"
+      case Fun(ts)       =>
+        var str: String = "Expr["
+        if(ts.length > 2) str += "("
+        str += ts.init.mkString(", ")
+        if(ts.length > 2) str += ")"
+        str += "] => Expr[" + ts.last.toString + "]"
+        str
+    }
+  }
+
+  override def toString: String = stringVal(this)
 }
-case class TupleType(ts: List[ParserType]) extends ParserType
+
 case class VarP(s: String) extends ParserType
+case class TupleType(ts: List[ParserType]) extends ParserType
 case class Fun(ts: List[ParserType]) extends ParserType
 
 object Fun {

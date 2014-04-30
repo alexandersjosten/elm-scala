f7f5d95dea11ca72a7201af2f5021072a1469265
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-04-30 14:36:27 +0200

    toString methods added to produce correct code

--- src/main/scala/elm/ParserTypes.scala
+++ src/main/scala/elm/ParserTypes.scala
@@ -1,21 +1,41 @@
 package elm
 
 sealed abstract class ParserType {
-  implicit def stringVal(pt: ParserType): String = {
+  def stringVal(pt: ParserType): String = {
     pt match {
-      case VarP(s)       => s
-      case TupleType(ts) => "(" + ts.mkString(",") + ")"
       case Fun(ts)       =>
         var str: String = "Expr["
         if(ts.length > 2) str += "("
-        str += ts.init.mkString(", ")
+        str += ts.init.mkString(",")
         if(ts.length > 2) str += ")"
-        str += "] => Expr[" + ts.last.toString + "]"
+        str += " => " + ts.last.toString + "]"
         str
+      case other         => stringValAux(other)
+    }
+  }
+  /*
+   Expr[(Expr[a => b],Signal a) => Signal b]
+   (a -> b) -> Signal a -> Signal b
+   Expr[((A => B), Signal[A]) => Signal[B]]
+   */
+  def stringValAux(pt: ParserType): String = {
+    pt match {
+      case VarP(s)       =>
+        if(s contains " ") {
+          val index = s.indexOf(" ")
+          s.substring(0, index) + "[" + s.substring(index + 1).toUpperCase.trim + "]"
+        }
+        else {
+          s.capitalize
+        }
+      case TupleType(ts) => "(" + ts.mkString(",") + ")"
+      case Fun(ts)       =>
+        "(" + ts.mkString(" => ") + ")"
+      case App(l, r)     => l.toString + "[" + r.toString + "]"
     }
   }
 
-  override def toString: String = stringVal(this)
+  override def toString: String = stringValAux(this)
 }
 
 case class VarP(s: String) extends ParserType

523b2bfeafa37f33e6a76f1fa8389a5bbd60be5b
David Spångberg <david@tunna.org>
2014-04-30 16:45:43 +0200

    Update elm AST to convert to javascript AST and later into a string

--- src/main/scala/elm/Elm.scala
+++ src/main/scala/elm/Elm.scala
@@ -17,20 +17,19 @@ class Program {
   def emit: String = {
     val str = new StringBuilder
     str ++= Constants.ElmHeader
-    str ++= indent(3, "var _op = {};\n")
 
+    val elmJs: ElmJs = new ElmJs()
     val prog = genProgram
 
-    prog.foreach { str ++= indent(3, _) }
-
-    str ++= indent(3, "_elm.Main.values = {_op: _op")
-
     prog.foreach {
-      case InitS(n, _) =>
-        str ++= "\n" + indent(22, "," + n + ": " + n)
-      case ImportS(_, _) => ()
+      case InitS(n, e) =>
+        elmJs.addDef(n, e)
+      case _ => ()
     }
-    str ++= "};\n" + Constants.ElmFooter
+
+    str ++= elmJs.emit
+
+    str ++= "\n" + Constants.ElmFooter
     str.toString
   }
 
@@ -79,12 +78,86 @@ class Elm {
   def infix_*(a: Expr[Int], b: Expr[Int]) = BinOpE(MulB(), a, b)
   def infix_/(a: Expr[Int], b: Expr[Int]) = BinOpE(DivB(), a, b)
 
+  implicit def expToExp[A](e: Expr[A]): Expr[A] = e
+  implicit def unitToExp(e: Unit): Expr[Unit] = UnitE()
   implicit def intToExp(i: Int): Expr[Int] = NumE(i)
   implicit def stringToExp(s: String): Expr[String] = StringE(s)
-  implicit def tup2ToExp[A,B](p: (A, B))
+
+  implicit def tup2ToExp1[A,B](p: (A, B))
     (implicit fa: A => Expr[A], fb: B => Expr[B])
       : Expr[(A, B)] = Tup2E(p._1, p._2)
+
+  implicit def tup2ToExp2[A,B](p: (Expr[A], B))
+    (implicit fb: B => Expr[B])
+      : Expr[(A, B)] = Tup2E(p._1, p._2)
+
+  implicit def tup2ToExp3[A,B](p: (A, Expr[B]))
+    (implicit fa: A => Expr[A])
+      : Expr[(A, B)] = Tup2E(p._1, p._2)
+
+  implicit def tup2ToExp4[A,B](p: (Expr[A], Expr[B]))
+      : Expr[(A, B)] = Tup2E(p._1, p._2)
+
   implicit def tup3ToExp[A,B,C](p: (A, B, C))
     (implicit fa: A => Expr[A], fb: B => Expr[B], fc: C => Expr[C])
       : Expr[(A, B, C)] = Tup3E(p._1, p._2, p._3)
+
+}
+
+class ElmJs {
+  import scala.collection.mutable.MutableList
+
+  private val defs: MutableList[(String, JsExpr)] =
+    new MutableList()
+
+  defs += (("_op", JsObject()))
+
+  def addDef[A](n: String, e: JsExpr): Unit = defs += ((n, e))
+
+  def emit: String = {
+    val elmMain = JsAssignment("Elm.Main", JsBinOp("Elm.Main", "||", JsObject()))
+
+    val makeBody = {
+      JsStr("use strict") &
+      JsAssignment("_elm.Main", JsBinOp("_elm.Main", "||", JsObject())) &
+      JsIf("_elm.Main.values", new JsReturn("_elm.Main.values")) &
+      JsDef("_N", "Elm.Native") &
+      JsDef("_U", "_N.Utils.make(_elm)") &
+      JsDef("_L", "_N.List.make(_elm)") &
+      JsDef("_E", "_N.Error.make(_elm)") &
+      JsDef("_J", "_N.JavaScript.make(_elm)") &
+      JsDef("$moduleName", JsStr("Main")) &
+      JsDef("Basics", "Elm.Basics.make(_elm)") &
+      JsDef("Color", "Elm.Color.make(_elm)") &
+      JsDef("Graphics", JsBinOp("Graphics", "||", JsObject())) &
+      JsAssignment("Graphics.Collage", JsFunctionCall("Elm.Graphics.Collage.make", "_elm")) &
+      JsDef("Graphics", JsBinOp("Graphics", "||", JsObject())) &
+      JsAssignment("Graphics.Element", JsFunctionCall("Elm.Graphics.Element.make", "_elm")) &
+      JsDef("List", JsFunctionCall("Elm.List.make", "_elm")) &
+      JsDef("Maybe", JsFunctionCall("Elm.Maybe.make", "_elm")) &
+      JsDef("Mouse", JsFunctionCall("Elm.Mouse.make", "_elm")) &
+      JsDef("Native", JsBinOp("Native", "||", JsObject())) &
+      JsAssignment("Native.Ports", JsFunctionCall("Elm.Native.Ports.make", "_elm")) &
+      JsDef("Signal", JsFunctionCall("Elm.Signal.make", "_elm")) &
+      JsDef("String", JsFunctionCall("Elm.String.make", "_elm")) &
+      JsDef("Text", JsFunctionCall("Elm.Text.make", "_elm")) &
+      JsDef("Time", JsFunctionCall("Elm.Time.make", "_elm")) &
+      jsDefs &
+      new JsReturn("_elm.Main.values")
+    }
+
+    val makeFun = JsAssignment("Elm.Main.make",
+      JsAnonymousFunction(List("_elm"), makeBody))
+
+    JsPrettyPrinter.prettyStmt(elmMain & makeFun)
+  }
+
+  private def jsDefs: JsStmt = {
+    def toDef(x: (String, JsExpr)): JsStmt = JsDef(x._1, x._2)
+    def toTup(x: (String, JsExpr)): (JsName, JsExpr) = (x._1, x._1)
+
+    defs.tail.foldLeft(toDef(defs.head))(_ & toDef(_)) &
+    JsAssignment("_elm.Main.values", JsObject(defs.map(toTup).toList))
+  }
+
 }
--- src/main/scala/elm/Types.scala
+++ src/main/scala/elm/Types.scala
@@ -2,17 +2,15 @@ package elm
 
 sealed case class Signal[T]()
 sealed case class Element()
-sealed case class Var[T](name: String) {
-  override def toString() = Var.stringVal(this)
-}
+sealed case class Var[T](name: String)
 
 object Var {
-  implicit def stringVal[A](v: Var[A]): String = v.name
+  implicit def toJs(v: Var[Any]) = JsName(v.name)
 }
 
 // Expr AST stuff
 sealed abstract class Expr[+A] {
-  override def toString() = Expr.stringVal(this)
+  def toJs() = Expr.toJs(this)
 }
 
 case class UnitE() extends Expr[Unit]
@@ -27,9 +25,6 @@ case class App2E[A, B, C](e1: Expr[(A, B) => C], e2: Expr[A],
                           e3: Expr[B]) extends Expr[C]
 case class BinOpE(op: BinOp, e1: Expr[Int], e2: Expr[Int])
     extends Expr[Int]
-case class InputSignalE[A](i: Int) extends Expr[Signal[A]]
-case class LiftE1[A, B](e1: Expr[A => Signal[B]], e2: Expr[A])
-    extends Expr[Signal[B]]
 case class BuiltInE[A](v: Var[A]) extends Expr[A]
 case class Tup2E[A, B](e1: Expr[A], e2: Expr[B])
     extends Expr[(A, B)]
@@ -37,31 +32,41 @@ case class Tup3E[A, B, C](e1: Expr[A], e2: Expr[B], e3: Expr[C])
     extends Expr[(A, B, C)]
 
 object Expr {
-  implicit def stringVal[A](e: Expr[A]): String = e match {
-    case UnitE()            => "()"
-    case NumE(n)            => n.toString
-    case StringE(s)         => "\"" + s + "\""
-    case VarE(v)            => v.toString
-    case Lam1E(v, e)        => "function (" + v + ") { return " + e + "; }"
-    case App1E(e1, e2)      => e1 + "(" + e2 + ")"
-    case App2E(e1, e2, e3)  => appE(e1, e2, e3)
-    case BinOpE(op, e1, e2) => e1 + op + e2
-    case BuiltInE(v)        => v.toString
-    case Tup2E(a, b)        => tup(a, b)
-    case Tup3E(a, b, c)     => tup(a, b, c)
+  implicit def toJs(e: Expr[Any]): JsExpr = e match {
+    case UnitE() => JsUnit()
+    case NumE(n) => JsNum(n)
+    case StringE(s) => JsStr(s)
+    case VarE(Var(n)) => JsName(n)
+    case Lam1E(v, e) => JsAnonymousFunction(List(v), JsReturn(Some(e)))
+    case App1E(f, e) => JsFunctionCall(f, e)
+    case App2E(f, e1, e2) => appExpr(f, e1, e2)
+    case BinOpE(op, e1, e2) => JsBinOp(e1, op, e2)
+    case BuiltInE(v) => v
+    case Tup2E(e1, e2) => tupExpr(e1, e2)
+    case x => JsName(x.toString)
   }
 
-  private def appE(e: Expr[Any], es: Expr[Any]*) = {
-    "A" + es.size + "(" + e + ", " + es.mkString(", ") + ")"
-  }
+  private def tupExpr(es: Expr[Any]*) = {
+    import scala.collection.mutable.ListBuffer
+    val args: ListBuffer[(JsName, JsExpr)] = ListBuffer()
+    args += ((JsName("ctor"), JsStr("_Tuple" + es.size)))
 
-  private def tup(es: Expr[Any]*) = {
-    var s = "{ ctor: \"_Tuple" + es.size + "\""
-    for (i <- 0 to es.size-1) {
-      s += ",_" + i + ": " + es(i) + " "
+    var n: Int = -1
+    es foreach { e =>
+      n += 1
+      args += ((JsName("_" + n), e))
     }
-    s += "}"
-    s
+
+    JsObject( args.result )
+  }
+
+  private def appExpr(e: Expr[Any], es: Expr[Any]*) = {
+    import scala.collection.mutable.ListBuffer
+    val args: ListBuffer[JsExpr] = ListBuffer()
+    args += e
+    es foreach (args += _)
+
+    JsFunctionCall("A" + es.size, args:_*)
   }
 
   implicit def expToFunc2[A,B,C](f: Expr[(A,B) => C]): Func2[A, B, C] = Func2(f)
@@ -78,22 +83,19 @@ sealed case class Func2[A, B, C](expr: Expr[(A, B) => C]) {
 }
 
 // Binary operators for type Expr
-sealed abstract class BinOp {
-  override def toString() = BinOp.stringVal(this)
-}
-
+sealed abstract class BinOp
 case class AddB() extends BinOp
 case class SubB() extends BinOp
 case class MulB() extends BinOp
 case class DivB() extends BinOp
 
 object BinOp {
-  implicit def stringVal(op: BinOp): String = op match {
+  implicit def toJs(b: BinOp): JsName = JsName(b match {
     case AddB() => "+"
     case SubB() => "-"
     case MulB() => "*"
     case DivB() => "/"
-  }
+  })
 }
 
 // SimpleType AST stuff

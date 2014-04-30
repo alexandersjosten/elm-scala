34f6956853eddd2a07a602ecc5a949d1e49e14cd
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-04-30 14:34:46 +0200

    ParseElmModule, code generation added

--- src/main/scala/elm/ParseElmModule.scala
+++ src/main/scala/elm/ParseElmModule.scala
@@ -3,5 +3,14 @@ package elm
 import ElmParser._
 
 object ParseElmModule {
-  def parseModule(em: ElmModule): String = ""
+  def parseModule(em: ElmModule): String = {
+    var retStr: String = "package elm\n\n"
+    retStr += "object " + em.name + " {\n"
+    em.functions.foreach((fd: FunDef[ParserType]) =>
+      retStr += "def " + fd.s + ": " + fd.ty.stringVal(fd.ty) + "\n"
+    )
+    retStr += "}\n"
+    retStr
+  }
 }
+

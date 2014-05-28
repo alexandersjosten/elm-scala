464429908b0f2f0e771e56f2136424817720386f
David Spångberg <david@tunna.org>
2014-05-28 16:06:49 +0200

    add functions to scala prelude lib

--- src/main/scala/elm/lib/Prelude.scala
+++ src/main/scala/elm/lib/Prelude.scala
@@ -3,4 +3,9 @@ package elm
 object Prelude {
   def lift[A, B]: Expr[((A => B), Signal[A]) => Signal[B]] =
     BuiltInE(Var("Signal.lift"))
+
+  def lift2[A, B, C]: Expr[(((A, B) => C), Signal[A], Signal[B]) => Signal[C]]
+    = BuiltInE(Var("Signal.lift2"))
+
+  def max[A]: Expr[(A, A) => A] = BuiltInE(Var("Basics.max"))
 }

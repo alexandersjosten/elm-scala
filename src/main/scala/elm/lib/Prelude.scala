735e6e044d94c071564a34117f0a337a6e5bfcb4
David Spångberg <david@tunna.org>
2014-05-28 18:44:54 +0200

    add alot of crap to prelude (need to use parser instead...)

--- src/main/scala/elm/lib/Prelude.scala
+++ src/main/scala/elm/lib/Prelude.scala
@@ -1,5 +1,11 @@
 package elm
 
+class Time
+class Path
+class LineStyle
+class Form
+class Shape
+
 object Prelude {
   def lift[A, B]: Expr[((A => B), Signal[A]) => Signal[B]] =
     BuiltInE(Var("Signal.lift"))
@@ -8,4 +14,44 @@ object Prelude {
     = BuiltInE(Var("Signal.lift2"))
 
   def max[A]: Expr[(A, A) => A] = BuiltInE(Var("Basics.max"))
+
+  def image: Expr[(Int, Int, String) => Element] = BuiltInE(Var("Graphics.Element.image"))
+
+  def collage: Expr[(Int, Int, List[Form]) => Element]
+    = BuiltInE(Var("Graphics.Collage.collage"))
+
+  def filled: Expr[(Color, Shape) => Form] = BuiltInE(Var("Graphics.Collage.filled"))
+
+  def ngon: Expr[(Int, Int) => Shape] = BuiltInE(Var("Graphics.Collage.ngon"))
+
+  def segment: Expr[((Int,Int), (Int,Int)) => Path]
+    = BuiltInE(Var("Graphics.Collage.segment"))
+
+  def traced: Expr[(LineStyle, Path) => Form]
+    = BuiltInE(Var("Graphics.Collage.traced"))
+
+  def solid: Expr[Color => LineStyle]
+    = BuiltInE(Var("Graphics.Collage.solid"))
+
+  def outlined: Expr[(LineStyle, Shape) => Form]
+    = BuiltInE(Var("Graphics.Collage.outlined"))
+
+  def cos: Expr[Int => Int]
+    = BuiltInE(Var("Basics.cos"))
+
+  def sin: Expr[Int => Int]
+    = BuiltInE(Var("Basics.sin"))
+
+  def degrees: Expr[Int => Int]
+    = BuiltInE(Var("Basics.degrees"))
+
+  def inSeconds: Expr[Int => Int]
+    = BuiltInE(Var("Time.inSeconds"))
+
+  def second: Expr[Int]
+    = BuiltInE(Var("Time.second"))
+
+  def every: Expr[Int => Signal[Int]]
+    = BuiltInE(Var("Time.every"))
+
 }

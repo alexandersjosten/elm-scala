49eaf6c41d7f7a9176e44865f4c9e6510fafca9e
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-05-28 16:26:04 +0200

    added yogi example

--- src/main/scala/elm/Main.scala
+++ src/main/scala/elm/Main.scala
@@ -7,12 +7,18 @@ import java.io.File
 object Main {
 
   def main(args: Array[String]) {
-    object Example extends Elm {
+    object Example1 extends Elm {
       var a = (x: Expr[Int]) => x + 1
       var main = lift(a, Mouse.x)
     }
 
-    val progStr = Example.prog.emit
+    object Example2 extends Elm {
+      var edgeLength = lift2(max[Int], Mouse.x, Mouse.y)
+      var resizeableYogi = (n: Expr[Int]) => image(n, n, "./yogi.jpg")
+      var main = lift(resizeableYogi, edgeLength)
+    }
+
+    val progStr = Example2.prog.emit
 
     printToFile(new File("target/elmtest.html"))(_.println(progStr))
     println(progStr)
